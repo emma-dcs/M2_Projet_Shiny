c3976a82da0cfe7f3ba93471ab78cd57c550a6e4
Maud Lesage <maud.lesage@agrocampus-ouest.fr>
2024-09-26 18:44:43 +0200

    code R nettoyage des données

--- Code_R.R
+++ Code_R.R
@@ -1,3 +1,26 @@
 library(data.table)
 
 economy <- fread("Glob_Economy_Ind.csv", sep = ",", header = TRUE)
+summary(economy)
+head(economy)
+
+#savoir combien il y a de données manquantes dans chaque variable
+na_counts <- sapply(economy, function(x) sum(is.na(x)))
+print(na_counts)
+
+
+# suppression des colonnes inutiles :
+# CountryID
+# Agriculture, hunting, forestry, fishing
+# IMF based exchange rate (car identique a AMA exchange rate)
+# changes in inventories
+
+economy <- economy[, -c(1, 5, 9, 10)]
+head(economy)
+
+#transformer les variables year, currency et country en factor
+economy$Year <- as.factor(economy$Year)
+economy$Country <- as.factor(economy$Country)
+economy$Currency <- as.factor(economy$Currency)
+summary(economy)
+
