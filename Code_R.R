bc2b00f0afe47938cfb348685213453c15ea2976
Emma DA COSTA SILVA <emma.dacostasilva@agrocampus-ouest.fr>
2024-09-27 16:25:25 +0200

    Modification des variables

--- Code_R.R
+++ Code_R.R
@@ -11,11 +11,10 @@ print(na_counts)
 
 # suppression des colonnes inutiles :
 # CountryID
-# Agriculture, hunting, forestry, fishing
-# IMF based exchange rate (car identique a AMA exchange rate)
-# changes in inventories
+# AMA exchange rate (car identique a IMF based exchange rate)
+# changes in inventories (car il n'y a pas de données)
 
-economy <- economy[, -c(1, 5, 9, 10)]
+economy <- economy[, -c(1, 4, 10)]
 head(economy)
 
 #transformer les variables year, currency et country en factor
@@ -24,3 +23,19 @@ economy$Country <- as.factor(economy$Country)
 economy$Currency <- as.factor(economy$Currency)
 summary(economy)
 
+#modifier le nom des colonnes
+colnames(economy) <- c("Pays", "Annee", "Taux_change", "Population", "Monnaie", 
+                       "RNB_hab", "VA_agri", "VA_construction", "Exportation", "Depense_tot", "Depense_gouv", 
+                       "Capital", "Capital_fixe", "Depense_menage", "Importation", "VA_manufacture", "VA_public", "VA_autre",
+                       "VA_tot", "VA_transp", "VA_commerce", "RNB", "PIB")
+
+#changer l'ordre des variables
+library(dplyr)
+
+economy <- economy %>%
+  select(Pays, Annee, Population, Monnaie, Taux_change, PIB, RNB, RNB_hab, 
+         Capital, Capital_fixe, Exportation, Importation, Depense_tot, 
+         Depense_gouv, Depense_menage, VA_tot, VA_agri, VA_commerce, 
+         VA_construction, VA_manufacture, VA_public, VA_transp, VA_autre)
+
+print(economy)
