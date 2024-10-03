5e4a35c52d1c9e89ee22c8cb31d830e74477551e
Maud Lesage <maud.lesage@agrocampus-ouest.fr>
2024-10-03 17:26:39 +0200

    graph donnees temporelles et carte en fct var

--- app.shiny/server.R
+++ app.shiny/server.R
@@ -7,26 +7,56 @@
 #    https://shiny.posit.co/
 #
 
-library(shiny)
-
-# Define server logic required to draw a histogram
 function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = input$color, border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = input$title)
-
-    })
+  
+  # Filtrer les données pour le pays sélectionné
+  country_data <- reactive({
+    economy %>%
+      filter(Pays == input$country)
+  })
+  
+  # Générer un graphique de l'évolution dans le temps d'une variable sélectionnée
+  output$timePlot <- renderPlot({
+    var_selected <- input$variable
     
-    output$summary <- renderPrint({
-      summary(faithful)
-    })
-
+    # Utilisation du sous-ensemble traditionnel au lieu de `.data`
+    ggplot(country_data(), aes(x = Annee, y = !!sym(var_selected), group = 1)) +
+      geom_line(color = "blue") +
+      geom_point(color = "red") +
+      labs(title = paste("Évolution de", var_selected, "pour", input$country),
+           x = "Année", y = var_selected) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  })
+  
+  # Carte interactive avec Leaflet
+  output$map <- renderLeaflet({
+    # Choix de la variable pour la palette
+    var_selected <- input$variable
+    
+    # Mise à jour du jeu de données pour inclure uniquement les pays avec des latitudes et longitudes non manquantes
+    economy_pays <- economy %>%
+      group_by(Pays) %>%
+      slice(1) %>%
+      filter(!is.na(lon) & !is.na(lat))
+    
+    # Générer une palette de couleurs basée sur la variable sélectionnée
+    palette <- colorNumeric(palette = "YlOrRd", domain = economy_pays[[var_selected]], na.color = "transparent")
+    
+    # Créer la carte avec les cercles colorés selon la variable sélectionnée
+    leaflet(economy_pays) %>%
+      addTiles() %>%
+      addCircleMarkers(
+        ~lon, ~lat,
+        popup = ~paste("Pays:", Pays, "<br>", var_selected, ":", economy_pays[[var_selected]]),
+        color = ~palette(economy_pays[[var_selected]]),
+        radius = 5,
+        stroke = FALSE, fillOpacity = 0.8
+      ) %>%
+      addLegend("bottomright", pal = palette, values = economy_pays[[var_selected]],
+                title = paste(var_selected, "(USD)"),
+                opacity = 1)
+  })
 }
+
+
