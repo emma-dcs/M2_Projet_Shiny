0c164767e9ec1885b6b2545a886257c154f2a28d
Maud Lesage <maud.lesage@agrocampus-ouest.fr>
2024-10-06 10:55:19 +0200

    ajout partie clustering

--- app.shiny/ui.R
+++ app.shiny/ui.R
@@ -8,38 +8,72 @@
 #
 
 
-
-# Define UI for application that draws a histogram
 fluidPage(
-  titlePanel("Visualisation des indicateurs économiques"),
+  titlePanel("Visualisation des indicateurs économiques et Clustering"),
   
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("country", "Choisissez un pays :", choices = unique(economy$Pays)),
-      selectInput("variable", "Choisissez une variable :", 
-                  choices = c("PIB", "RNB", "Population", "Taux_change", 
-                              "Capital", "Exportation", "Importation")),
-      selectInput("palette", "Choisissez une palette:", 
-                  choices = c("viridis", "magma", "plasma", "inferno", "cividis")),
-      selectInput("chartType", "Type de visualisation :", 
-                  choices = c("Graphique temporel", "Carte avec Leaflet", "Topogram")),
-      selectInput("year", "Choisissez une année :", choices = unique(economy$Annee))
+  # Organiser l'application en onglets
+  tabsetPanel(
+    
+    # Onglet descriptif
+    tabPanel(
+      "Descriptif",
       
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("country", "Choisissez un pays :", choices = unique(economy$Pays)),
+          selectInput("variable", "Choisissez une variable :", 
+                      choices = c("PIB", "RNB", "Population", "Taux_change", 
+                                  "Capital", "Exportation", "Importation")),
+          selectInput("palette", "Choisissez une palette:", 
+                      choices = c("viridis", "magma", "plasma", "inferno", "cividis")),
+          selectInput("chartType", "Type de visualisation :", 
+                      choices = c("Graphique temporel", "Carte avec Leaflet", "Topogram")),
+          selectInput("year", "Choisissez une année :", choices = unique(economy$Annee))
+        ),
+        
+        mainPanel(
+          conditionalPanel(
+            condition = "input.chartType == 'Graphique temporel'",
+            plotOutput("timePlot")
+          ),
+          conditionalPanel(
+            condition = "input.chartType == 'Carte avec Leaflet'",
+            leafletOutput("map", height = 600)
+          ),
+          conditionalPanel(
+            condition = "input.chartType == 'Topogram'",
+            plotOutput("topogramPlot")
+          )
+        )
+      )
     ),
     
-    mainPanel(
-      conditionalPanel(
-        condition = "input.chartType == 'Graphique temporel'",
-        plotOutput("timePlot")
-      ),
-      conditionalPanel(
-        condition = "input.chartType == 'Carte avec Leaflet'",
-        leafletOutput("map", height = 600)  # Espace réservé pour la carte
-      ),
-      conditionalPanel(
-        condition = "input.chartType == 'Topogram'",
-        plotOutput("topogramPlot")  # Espace réservé pour le topogram
+    # Onglet de clustering
+    tabPanel(
+      "Clustering",
+      
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("cluster_vars", "Variables pour le clustering :", 
+                      choices = c("PIB", "RNB", "Population", "Taux_change", 
+                                  "Capital", "Exportation", "Importation"),
+                      multiple = TRUE),
+          numericInput("num_clusters", "Nombre de clusters :", value = 3, min = 2, max = 10),
+          actionButton("run_cluster", "Lancer le clustering")
+        ),
+        
+        mainPanel(
+          plotOutput("clusteringPlot", height = 500),
+          tableOutput("cluster_summary"),
+          fluidRow(
+            box(tableOutput("country_clusters"), width = 12)
+          )
+        )
       )
     )
   )
 )
+
+
+
+
